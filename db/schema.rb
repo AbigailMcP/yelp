9645b7182522a2bc9066756fccbcd5f6af5bd0b5
Joseph <joseph.jr@gmail.com>
2016-10-07 09:44:24 +0100

    correct fb callback route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006115018) do
+ActiveRecord::Schema.define(version: 20161006112816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20161006115018) do
     t.integer  "user_id"
     t.float    "latitude"
     t.float    "longitude"
-    t.text     "address"
+    t.string   "address"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
